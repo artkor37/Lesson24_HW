22573561a23ec431fc012d8c33df587cfc10b1ed
artkor37 <artem.korolev037@gmail.com>
2024-03-09 15:35:21 +0200

    Main is changed

--- Lesson24_HW/src/Main.java
+++ Lesson24_HW/src/Main.java
@@ -4,131 +4,120 @@ import java.util.TreeMap;
 
 public class Main {
 
-            public static final String CORRECT_NUMBER = "[\\d()+ -]+";
-            public static final String CORRECT_NAME = "[А-я ]+";
-            private static boolean isContinue = false;
-            private static final Map<String, String> phoneBook = new TreeMap<>();
+    public static final String NUMBER = "(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})";
+    public static final String NAME = "[А-я ]+";
+    private static final Map<String, String> phoneBook = new TreeMap<>();
 
-            public static void main(String[] args) {
-                System.out.println("\t***Телефонный справочник***");
-                System.out.println("Вводите номер телефона в формате +7*** *** ** ** или 8*** *** ** **" +
-                        "\nИмя русскими буквами" +
-                        "\nКоманды: \"list\" - распечатать список абонентов и \"stop\" - остановить программу\n");
-                while (!isContinue) {
-                    System.out.print("Введите имя, номер или команду:");
-                    String input = new Scanner(System.in).nextLine();
-                    inputCheck(input);
+    public static void main(String[] args) {
 
-                }
-            }
+        System.out.println("\t***Телефонный справочник***");
+        System.out.println("Вводите номер телефона в формате +7*** *** ** ** или 8*** *** ** **" +
+                "\nИмя вводите русскими буквами" +
+                "\nКоманды: \"LIST\" - распечатать список абонентов и \"STOP\" - остановить программу" +
+                "\n\"BACK\" - вернуться в начало программы\n");
 
-            public static void inputCheck(String input) {
-                if (input.equalsIgnoreCase("stop")) {
-                    System.out.println("Спасибо, что воспользовались нашим телефонным справочником." +
-                            "\nДо свидания");
-                    isContinue = true;
-                } else if (input.equalsIgnoreCase("list")) {
-                    printAll();
-                } else if (input.matches(CORRECT_NUMBER)) {
-                    numberCheck(input);
-                } else if (input.matches(CORRECT_NAME)) {
-                    addName(input);
-                } else
+        while (true) {
+            System.out.print("Введите имя, номер или команду: ");
+            String input = new Scanner(System.in).nextLine().trim();
+            if (input.equalsIgnoreCase("stop")) {
+                System.out.println("\nСпасибо, что воспользовались нашим телефонным справочником." +
+                        "\n\t\t\t\tДо свидания!");
+                return;
+            } else if (input.equalsIgnoreCase("list")) {
+                printAll();
+            } else if (input.matches(NAME)) {
+                nameCheck(input);
+            } else {
+                String number = numberCheck(input);
+                if (number.equals("wrong")) {
                     wrongInput();
-            }
-
-            public static void add(String name, String number) {
-                phoneBook.put(name, number);
-                System.out.println("Имя " + name + " внесено в справочник на номер " + number);
-            }
-
-            public static void addName(String inputName) {
-                String name = inputName.substring(0, 1).toUpperCase() + inputName.substring(1);
-                if (phoneBook.containsKey(name)) {
-                    System.out.println(name + " - " + phoneBook.get(name));
                 } else {
-                    System.out.println("Имени " + "\"" + name + "\"" + " нет в справочнике. Введите номер для этого имени:");
-                    while (true) {
-                        String number = new Scanner(System.in).nextLine().trim();
-                        if (number.equalsIgnoreCase("back")) {
-                            return;
-                        }
-                        number = number.replaceAll("\\D+", "");
-                        if (number.length() == 11 && (number.charAt(0) == '7' || number.charAt(0) == '8')) {
-                            number = number.replaceAll("(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+7($2)$3-$4-$5");
-                            System.out.println(number);
-                            if (phoneBook.containsValue(number)) {
-                                System.out.println("Такой номер уже есть в справочнике" +
-                                        "\nВведите другой номер или команду \"back\", чтобы вернуться в начало");
-                            } else {
-                                add(name, number);
-                                return;
-                            }
-                        } else {
-                            wrongInput();
-                            System.out.println("Введите номер еще раз или команду \"back\", чтобы вернуться в начало\"");
-                        }
-                    }
-                }
-            }
-
-            public static void addNumber(String number) {
-                if (phoneBook.containsValue(number)) {
-                    String name = "";
-                    for (String name2 : phoneBook.keySet()) {
-                        if (phoneBook.get(name2).equalsIgnoreCase(number)) {
-                            name = name2;
-                        }
-                    }
-                    System.out.println(name + " - " + number);
-
-                } else {
-                    System.out.println("Номера " + number + " нет в справочнике. Введите имя для этого номера:");
-                    while (true) {
-                        String name = new Scanner(System.in).nextLine().trim();
-                        if (name.matches(CORRECT_NAME)) {
-                            name = name.substring(0, 1).toUpperCase() + name.substring(1);
-                            add(name, number);
-                            //System.out.println("Номер " + number + " внесен в базу на имя " + name1);
-                            return;
-                        } else
-                            System.out.print("Введите имя русскими буквами: ");
-                    }
-                }
-            }
-
-            public static String numberNameCheck(String number) {
-                number = number.replaceAll("\\D+", "");
-                if (number.length() == 11 && (number.charAt(0) == '7' || number.charAt(0) == '8')) {
-                    number = number.replaceAll("(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+7($2)$3-$4-$5");
-                    System.out.println(number);
-                    return number;
-                } else
-                    wrongInput();
-                return number;
-            }
-
-            public static void numberCheck(String number) {
-                number = number.replaceAll("\\D+", "");
-                if (number.length() == 11 && (number.charAt(0) == '7' || number.charAt(0) == '8')) {
-                    number = number.replaceAll("(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+7($2)$3-$4-$5");
-                    System.out.println(number);
                     addNumber(number);
-                } else
-                    wrongInput();
-            }
-
-            public static void printAll() {
-                if (phoneBook.isEmpty()) {
-                    System.out.println("Список пуст!");
                 }
-                for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-                    System.out.println(entry.getKey() + " - " + entry.getValue());
-                }
-            }
-
-
-            public static void wrongInput() {
-                System.out.println("Неверный ввод!");
             }
         }
+    }
+
+    public static String numberCheck(String number) {
+        number = number.replaceAll("\\D+", "").trim();
+        if (number.length() == 11 && (number.charAt(0) == '7' || number.charAt(0) == '8')) {
+            return number.replaceAll(NUMBER, "+7($2)$3-$4-$5");
+        } else {
+            return "wrong";
+        }
+    }
+
+
+    public static void nameCheck(String inputName) {
+        String name = inputName.substring(0, 1).toUpperCase() + inputName.substring(1);
+        if (phoneBook.containsKey(name)) {
+            System.out.println("\t" + name + " - " + phoneBook.get(name));
+        } else {
+            System.out.println("Имени " + "\"" + name + "\"" + " нет в справочнике. Введите номер для этого имени:");
+            while (true) {
+                String number = new Scanner(System.in).nextLine().trim();
+                if (number.equalsIgnoreCase("back")) {
+                    return;
+                }
+                String number2 = numberCheck(number);
+                if (number2.equals("wrong")) {
+                    wrongInput();
+                    System.out.println("Введите номер ещё раз:");
+                } else if (phoneBook.containsValue(number2)) {
+                    System.out.println("Такой номер уже есть в справочнике" +
+                            "\nВведите другой номер:");
+                } else {
+                    add(name, number2);
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void add(String name, String number) {
+        if (phoneBook.containsKey(name)) {
+            phoneBook.put(name, number);
+            System.out.println("Имя " + name + " eсть в справочнике. Старый номер изменён на новый " + number);
+            return;
+        }
+        phoneBook.put(name, number);
+        System.out.println("Имя " + name + " внесено в справочник на номер " + number);
+    }
+
+
+    public static void addNumber(String number) {
+        if (phoneBook.containsValue(number)) {
+            for (String printName : phoneBook.keySet()) {
+                if (phoneBook.get(printName).equalsIgnoreCase(number)) {
+                    System.out.println("\t" + printName + " - " + number);
+                }
+            }
+        } else {
+            System.out.println("Номера " + number + " нет в справочнике. Введите имя для этого номера:");
+            while (true) {
+                String name = new Scanner(System.in).nextLine().trim();
+                if (name.equalsIgnoreCase("back")) {
+                    return;
+                } else if (name.matches(NAME)) {
+                    name = name.substring(0, 1).toUpperCase() + name.substring(1);
+                    add(name, number);
+                    return;
+                } else
+                    System.out.print("Введите имя русскими буквами: ");
+            }
+        }
+    }
+
+    public static void printAll() {
+        if (phoneBook.isEmpty()) {
+            System.out.println("Список пуст!");
+        }
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+            System.out.println("\t" + entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+    public static void wrongInput() {
+        System.out.println("Неверный ввод!");
+    }
+}
